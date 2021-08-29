a93d0e27e3a5db6f6bdb5ae3266250de91980635
borchard-dev <89462084+borchard-dev@users.noreply.github.com>
2021-08-29 19:37:01 +1000

    Update simplexml.php

--- simplexml.php
+++ simplexml.php
@@ -11,6 +11,6 @@ Text Domain: borchard
 
 
 <?php
-$xml=simplexml_load_file("https://steamcommunity.com/gid/41109792/memberslistxml/?xml=1") or die("Error: Cannot create object");
+$xml=simplexml_load_file("https://steamcommunity.com/gid/41109792/memberslistxml/?xml=1") || die("Error: Cannot create object");
 print_r($xml);
 ?>
