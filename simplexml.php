9ff2fc38eb718f5eb7126e5f1d016700a4413469
borchard-dev <89462084+borchard-dev@users.noreply.github.com>
2021-08-29 19:14:31 +1000

    Update simplexml.php

--- simplexml.php
+++ simplexml.php
@@ -1,3 +1,14 @@
+/*
+Plugin Name: simplexml
+Plugin URI: https://blakeborchard.com/wp-plugins
+Description: simple xml script to take information from x place and shit it out
+Version: 0.0.1
+Author: borchdev
+Author URI: https://blakeborchard.com
+License: GPLv2 or later
+Text Domain: borchard
+*/
+
 
 <?php
 $xml=simplexml_load_file("https://steamcommunity.com/gid/41109792/memberslistxml/?xml=1") or die("Error: Cannot create object");
